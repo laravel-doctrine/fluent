eee1bfcdde439828374162a8a739142a02452a20
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2017-07-04 22:08:52 +0200

    Add path loader to fluent driver

--- src/FluentDriver.php
+++ src/FluentDriver.php
@@ -6,9 +6,15 @@ use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\Common\Persistence\Mapping\Driver\MappingDriver;
 use Doctrine\ORM\Mapping\Builder\ClassMetadataBuilder;
 use Doctrine\ORM\Mapping\MappingException;
+use FilesystemIterator;
 use InvalidArgumentException;
 use LaravelDoctrine\Fluent\Builders\Builder;
 use LaravelDoctrine\Fluent\Mappers\MapperSet;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveRegexIterator;
+use ReflectionClass;
+use RegexIterator;
 
 class FluentDriver implements MappingDriver
 {
@@ -23,19 +29,36 @@ class FluentDriver implements MappingDriver
     protected $fluentFactory;
 
     /**
+     * The file extension of mapping documents.
+     *
+     * @var string
+     */
+    protected $fileExtension = '.php';
+
+    /**
      * Initializes a new FileDriver that looks in the given path(s) for mapping
      * documents and operates in the specified operating mode.
      *
      * @param string[] $mappings
+     * @param array    $paths
+     *
+     * @throws MappingException
      */
-    public function __construct(array $mappings = [])
+    public function __construct(array $mappings = [], array $paths = [])
     {
         $this->fluentFactory = function (ClassMetadata $metadata) {
             return new Builder(new ClassMetadataBuilder($metadata));
         };
 
         $this->mappers = new MapperSet();
-        $this->addMappings($mappings);
+
+        if (!empty($paths)) {
+            $this->loadPaths($paths);
+        }
+
+        if (!empty($mappings)) {
+            $this->addMappings($mappings);
+        }
     }
 
     /**
@@ -79,7 +102,73 @@ class FluentDriver implements MappingDriver
     }
 
     /**
+     * @param array $paths
+     *
+     * @throws MappingException
+     */
+    public function loadPaths(array $paths)
+    {
+        $includedFiles = [];
+
+        foreach ($paths as $path) {
+            if (!is_dir($path)) {
+                throw MappingException::fileMappingDriversRequireConfiguredDirectoryPath($path);
+            }
+
+            $iterator = new RegexIterator(
+                new RecursiveIteratorIterator(
+                    new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS),
+                    RecursiveIteratorIterator::LEAVES_ONLY
+                ),
+                '/^.+'.preg_quote($this->fileExtension).'$/i',
+                RecursiveRegexIterator::GET_MATCH
+            );
+
+            foreach ($iterator as $file) {
+                $sourceFile = $file[0];
+
+                if (!preg_match('(^phar:)i', $sourceFile)) {
+                    $sourceFile = realpath($sourceFile);
+                }
+
+                require_once $sourceFile;
+
+                $includedFiles[] = $sourceFile;
+            }
+
+            $declared = get_declared_classes();
+
+            foreach ($declared as $className) {
+                $rc = new ReflectionClass($className);
+                $sourceFile = $rc->getFileName();
+
+                if (!in_array($sourceFile, $includedFiles)) {
+                    continue;
+                }
+
+                if ($rc->isAbstract() || $rc->isInterface()) {
+                    continue;
+                }
+
+                if (!$rc->implementsInterface(Mapping::class)) {
+                    continue;
+                }
+
+                if ($this->isTransient($className)) {
+
+                    /** @var Mapping $mapping */
+                    $mapping = $rc->newInstanceWithoutConstructor();
+
+                    $this->addMapping($mapping);
+                }
+            }
+        }
+    }
+
+    /**
      * @param string[] $mappings
+     *
+     * @throws InvalidArgumentException
      */
     public function addMappings(array $mappings = [])
     {
